629661e83ede7f9fa939a49ce506440a062a9ba9
Andryss <andr.krivosheev@gmail.com>
2025-02-19 00:56:25 +0300

    Init

---
+++ src/test/java/ru/andryss/trousseau/TrousseauApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.trousseau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrousseauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
