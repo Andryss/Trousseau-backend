13f1450592cd0638ac678f1678634ead5ac31c48
Andryss <andr.krivosheev@gmail.com>
2025-03-08 13:17:38 +0300

    Change ItemDto

--- src/main/java/ru/andryss/trousseau/controller/ItemApiController.java
+++ src/main/java/ru/andryss/trousseau/controller/ItemApiController.java
@@ -1,5 +1,6 @@
 package ru.andryss.trousseau.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import ru.andryss.trousseau.generated.api.ItemApi;
 import ru.andryss.trousseau.generated.model.GetItemsResponse;
 import ru.andryss.trousseau.generated.model.ItemDto;
 import ru.andryss.trousseau.generated.model.ItemInfoRequest;
+import ru.andryss.trousseau.generated.model.ItemMediaDto;
 import ru.andryss.trousseau.model.ItemEntity;
 import ru.andryss.trousseau.service.ItemService;
 import ru.andryss.trousseau.service.MediaService;
@@ -56,8 +58,19 @@ public class ItemApiController implements ItemApi {
         return new ItemDto()
                 .id(entity.getId())
                 .title(entity.getTitle())
-                .media(mediaService.toUrls(entity.getMediaIds()))
+                .media(mapToDtos(entity.getMediaIds()))
                 .description(entity.getDescription())
                 .status(entity.getStatus().toOpenApi());
+    }
+
+    private List<ItemMediaDto> mapToDtos(List<String> ids) {
+        List<String> urls = mediaService.toUrls(ids);
+        List<ItemMediaDto> dtos = new ArrayList<>(ids.size());
+        for (int i = 0; i < ids.size(); i++) {
+            dtos.add(new ItemMediaDto()
+                    .id(ids.get(i))
+                    .href(urls.get(i)));
+        }
+        return dtos;
     }
 }
